9c7ad67a3b037af359f19d9de5ce6fe968e83d76
Amine-AD <daimallah.amine@gmail.com>
2023-07-07 13:05:49 +0800

    setup eslint and prettier for react

---
+++ src/components/Button2.tsx
@@ -0,0 +1,9 @@
+const Button2 = () => {
+  return (
+    <button type="button" className="px-4 h-12 inline-flex gap-4">
+      hello
+    </button>
+  );
+};
+
+export default Button2;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			primary: 'hsl(96 72% 67%)',
+			secondary: 'hsl(0 0% 11%)',
+			thirdary: 'hsl(0 0% 55%)',
+			muted: 'hsl(0 0% 73%)',
+			background: 'hsl(96 100% 3%)',
+			forground: 'hsl(0 0% 100%)',
+
+		},
 		extend: {},
 	},
 	plugins: [],
